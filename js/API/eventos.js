474c9fe3d658b91a3da0a12c44f208b796179598
Ivan Eric <ivaneric79@gmail.com>
2013-02-23 11:54:12 -0600

    Practica

---
+++ js/API/eventos.js
@@ -0,0 +1,35 @@
+//Eventos
+$(document).ready(function(e){
+	document.addEventListener("deviceready", function(){
+			if(!isLogin())
+		window.location.href = "#login";
+		
+	//Funcionalidad Login
+	$('#regSubmit').tap(function(){
+		if($('#regNombre').val()!='' && $('#regLugar').val()!='' && $('#regEmail').val()!='' && $('#regTel').val()!=''){
+			var nom=$('#regNombre').val();
+			var lug=$('#regLugar').val();
+			var ema=$('#regEmail').val();
+			var tel=$('#regTel').val();
+			
+			pgAlert("valores",nom+'\n'+lug+'\n'+ema+'\n'+tel);
+		}else{
+			pgAlert("Error",'Todos los campos son requeridos.');	
+		}
+	});
+		
+		//otros eventos de phonegap
+		//document.addEventListener("pause",funcion,false);
+		//document.addEventListener("resume",funcion,false);
+		//document.addEventListener("online",funcion,false);
+		//document.addEventListener("offline",funcion,false);
+		
+		
+		}, false);
+	
+
+});
+
+function isLogin(){
+	return false;
+}
---
+++ js/API/notificaciones.js
@@ -0,0 +1,12 @@
+// Notificaciones
+function pgAlert(tit,com){
+	navigator.notification.alert(com,null, tit, "Confirmado");
+	//otras notificaciones
+	//navigator.notification.confirm("comentario",function(boton){
+	//	if(boton==1){}},"titulo","si,no,tal vez");
+	//navigator.notification.beep(5);
+	//navigator.notification.vibrate(500);
+	
+	
+	}
+
